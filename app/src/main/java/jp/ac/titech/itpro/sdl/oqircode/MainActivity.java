dc085e1727bff0bee8a0df95576e40c564a5b8e1
shizuka <astaroth.meffi.me617@gmail.com>
2022-07-05 23:53:34 +0900

    フォーマット

--- app/src/main/java/jp/ac/titech/itpro/sdl/oqircode/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/oqircode/MainActivity.java
@@ -3,27 +3,30 @@ package jp.ac.titech.itpro.sdl.oqircode;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
+
 import jp.ac.titech.itpro.sdl.oqircode.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-private ActivityMainBinding binding;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-     binding = ActivityMainBinding.inflate(getLayoutInflater());
-     setContentView(binding.getRoot());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {

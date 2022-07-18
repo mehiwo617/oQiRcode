76499ca6140834cd2225b23a2218cdb8f92c2759
shizuka <astaroth.meffi.me617@gmail.com>
2022-07-18 22:35:13 +0900

    QRコードの設置

--- app/src/main/java/jp/ac/titech/itpro/sdl/oqircode/ui/gallery/GalleryFragment.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/oqircode/ui/gallery/GalleryFragment.java
@@ -1,33 +1,49 @@
 package jp.ac.titech.itpro.sdl.oqircode.ui.gallery;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.google.zxing.WriterException;
+
+import jp.ac.titech.itpro.sdl.oqircode.QRCreator;
 import jp.ac.titech.itpro.sdl.oqircode.databinding.FragmentGalleryBinding;
 
 public class GalleryFragment extends Fragment {
 
-private FragmentGalleryBinding binding;
+    private final static String TAG = GalleryFragment.class.getSimpleName();
+    private FragmentGalleryBinding binding;
+    private QRCreator mQRCreator = new QRCreator();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
+                             ViewGroup container, Bundle savedInstanceState) {
         GalleryViewModel galleryViewModel =
                 new ViewModelProvider(this).get(GalleryViewModel.class);
 
-    binding = FragmentGalleryBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
+        binding = FragmentGalleryBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        try {
+            ImageView imageViewQRcode = binding.imageQR;
+            imageViewQRcode.setImageBitmap(mQRCreator.createQR());
+        } catch (WriterException e) {
+            e.printStackTrace();
+            Log.e(TAG, "QRコード作成失敗");
+        }
+
+//        final TextView textView = binding.textGallery;
+//        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
-@Override
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;

2b405203ffed494575266300246327290792dbcc
shizuka <astaroth.meffi.me617@gmail.com>
2022-07-19 12:08:45 +0900

    Settingsの削除

--- app/src/main/java/jp/ac/titech/itpro/sdl/oqircode/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/oqircode/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -120,13 +119,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
     }
 
     @Override
